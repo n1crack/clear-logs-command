d2f2a8e4c1d9bd056345fe8982359633c6c6458d
Yusuf Özdemir <yusuf@ozdemir.be>
2023-12-09 15:31:10 +0300

    cleanup

--- src/Console/Commands/ClearLogs.php
+++ src/Console/Commands/ClearLogs.php
@@ -16,28 +16,19 @@ class ClearLogs extends Command
 
     public function handle()
     {
-        $defaultPath = storage_path('logs');
-        $path = $this->option('daily') ? $this->getDailyLogPath($defaultPath) : $defaultPath;
-
-        // Ensure the path is within the logs directory
-        if (!str_starts_with($path, $defaultPath)) {
-            $this->error('Invalid path. You can only clear logs within the logs directory.');
-            return;
-        }
+        $path = storage_path('logs');
 
         // Get all files in the logs directory
         $files = File::glob("{$path}/*.log");
 
+        if (!count($files)){
+            $this->info("No log files found.");
+            return;
+        }
+
         // Display verbose information if --verbose is used
         $verbosityLevel = $this->getOutput()->getVerbosity();
 
-        if ($verbosityLevel >= OutputInterface::VERBOSITY_VERBOSE) {
-            // show verbose messages
-            $this->line('ok');
-            foreach ($files as $file) {
-                $this->line($file);
-            }
-        }
         if ($verbosityLevel >= OutputInterface::VERBOSITY_VERBOSE || $this->option('dry-run')) {
             $this->info('Files to be cleared:');
             foreach ($files as $file) {
@@ -57,14 +48,5 @@ class ClearLogs extends Command
         }
 
         $this->info($this->option('daily') ? 'Daily logs cleared successfully!' : 'Logs cleared successfully!');
-    }
-
-    protected function getDailyLogPath($path)
-    {
-        // Convert path to daily log path
-        $date = now()->format('Y-m-d');
-        $filename = "laravel-{$date}.log";
-
-        return $path . DIRECTORY_SEPARATOR . $filename;
     }
 }
